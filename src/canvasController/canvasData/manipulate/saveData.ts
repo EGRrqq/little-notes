0dd4b4cc27abc296617f2526094f8950447714a5
EGRrqq <egrrqqdev@gmail.com>
2024-02-01 18:02:38 -0500

    feat(cache): use fetch to download data
    feat(cace): split funcions into small pieces
    - createAnchor
    - objToUrl / removeObjUrl

--- src/canvasController/canvasData/manipulate/saveData.ts
+++ src/canvasController/canvasData/manipulate/saveData.ts
@@ -8,16 +8,36 @@ type TSaveData = (
 ) => void;
 
 export const saveData: TSaveData = (data, filename, contentType) => {
+  fetch(objToUrl(data, contentType))
+    .then((res) => {
+      const a = createAnchor(res.url, filename);
+      a.click();
+
+      return res.url;
+    })
+    .then((url) => removeObjUrl(url))
+    .catch((err) => console.error("onSave:", err));
+};
+
+function createAnchor(url: string, filename: string): HTMLAnchorElement {
+  const a = document.createElement("a");
+
+  a.href = url;
+  a.download = filename;
+
+  return a;
+}
+
+function objToUrl(data: IData, contentType: TContentType): string {
   const dataBlob = new Blob([JSON.stringify(data, null, 2)], {
     type: contentType,
   });
+
   const dataUrl = URL.createObjectURL(dataBlob);
-  const a = document.createElement("a");
 
-  a.href = dataUrl;
-  a.download = filename;
-  a.click();
+  return dataUrl;
+}
 
-  URL.revokeObjectURL(dataUrl);
-  a.remove();
-};
+function removeObjUrl(url: string) {
+  URL.revokeObjectURL(url);
+}
